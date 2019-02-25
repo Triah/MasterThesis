e5e166632c682ad0bb400315583644e4a0766d04
Triah <nicolaihedegaardjensen93@gmail.com>
2019-02-26 00:48:56 +0100

    small changes, starting to work on a file creation tool for the js files needed for the builder

---
+++ MasterThesisPlatform/Util/ComponentFileUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterThesisPlatform.Util
+{
+    public class ComponentFileUtility
+    {
+        public ComponentFileUtility()
+        {
+            //TODO
+        }
+
+
+        public void addFileObjectToMongoDB(string stringifiedFile)
+        {
+            //TODO
+            //Needs File name, category, 
+            //filetype, probably a path for each server to fetch it 
+            //and the contents of the class so it can be generated.
+        }
+
+        //I need a method for sending the creation request to node, this will most likely be in the developer part
+
+        public string StringifyFile(string fileName) {
+            //TODO
+            return null;
+        }
+    }
+}
