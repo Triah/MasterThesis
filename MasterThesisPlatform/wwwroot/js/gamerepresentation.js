35eff21dc6e244872f42529d713749074874d2a6
Triah <nicolaihedegaardjensen93@gmail.com>
2019-03-26 03:11:13 +0100

    working on click event listener that adds all info to the options based on existing parameters, its gonna be a pretty difficult algorithm to get right

--- MasterThesisPlatform/wwwroot/js/gamerepresentation.js
+++ MasterThesisPlatform/wwwroot/js/gamerepresentation.js
@@ -28,11 +28,39 @@ canvas.onmousemove = function (e) {
     }
 }
 
+
+//This method is still in progress, its pretty difficult to get right though so might be a while till its done
 canvas.addEventListener('click', function (e) {
     for (var i = 0; i < canvasObjects.length; i++) {
         if (canvasObjects[i].getCollisionArea(e)) {
+            //get and reset div
             var div = document.getElementById("buildmode-options-elements");
-            div.innerHTML = "<p>" + canvasObjects[i].object + "</p>";
+            div.innerHTML = "";
+
+            for (var j = 0; j < Object.keys(canvasObjects[i]).length; j++) {
+                //Oh boy this is unreadable as fuck
+                if (typeof Object.values(canvasObjects[i])[j] === 'object') {
+                    for (var k = 0; k < Object.values(canvasObjects[i])[j].length; k++) {
+                        div.innerHTML += "<p><strong> bound </strong></p>";
+                        div.innerHTML += "<p id=" + '"' + "objectvalue" + k + '"' + "><p>"
+                        for (var n = 0; n < Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k]).length; n++) {
+                            document.getElementById("objectvalue" + k).innerHTML +=
+                                Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + ": " +
+                                Object.values(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + ", ";
+                        }
+                        
+                    }
+                    console.log(Object.values(canvasObjects[i])[j]);
+                    //console.log(Object.values(canvasObjects[i])[j])
+                } else if (typeof Object.values(canvasObjects[i])[j] === 'boolean') {
+                    console.log("bool detected");
+                }
+
+            }
+
+            div.innerHTML += "<p>" + canvasObjects[i].object + "</p>";
+            div.innerHTML += "<input id=" + '"' + "boundsTextfield" + '"' + " type=" + '"' + "text" + '"' + "><br>";
+            div.innerHTML += "<input id=" + '"' + "boundsTextfield" + '"' + " type=" + '"' + "text" + '"' + "><br>";
         }
     }
 }, false)
@@ -48,124 +76,139 @@ canvas.onmouseup = function (e) {
 function getObjects() {
     return canvasObjects;
 }
- function createObject(object){ 
-var catAndName = object.Category.toLowerCase() + object.ComponentName.charAt(0).toLowerCase() + object.ComponentName.substr(1) 
 
-if(catAndName == "abstractcollisionShape.js") { 
-object = new abstractcollisionShape(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+function createObject(object) {
+    var catAndName = object.Category.toLowerCase() + object.ComponentName.charAt(0).toLowerCase() + object.ComponentName.substr(1)
 
-else if(catAndName == "abstractshape.js") { 
-object = new abstractshape(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    if (catAndName == "abstractcollisionShape.js") {
+        object = new abstractcollisionShape(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "shapessquare.js") { 
-object = new shapessquare(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "abstractshape.js") {
+        object = new abstractshape(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "testssquare.js") { 
-object = new testssquare(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "shapessquare.js") {
+        object = new shapessquare(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "testsquare.js") { 
-object = new testsquare(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "testssquare.js") {
+        object = new testssquare(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "testcategorysquare.js") { 
-object = new testcategorysquare(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "testsquare.js") {
+        object = new testsquare(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "dwadwasquare.js") { 
-object = new dwadwasquare(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "testcategorysquare.js") {
+        object = new testcategorysquare(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "dwadwacollisionShape.js") { 
-object = new dwadwacollisionShape(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "dwadwasquare.js") {
+        object = new dwadwasquare(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "testscollisionShape.js") { 
-object = new testscollisionShape(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "dwadwacollisionShape.js") {
+        object = new dwadwacollisionShape(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "abstractsquare.js") { 
-object = new abstractsquare(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "testscollisionShape.js") {
+        object = new testscollisionShape(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "shapescollisionShape.js") { 
-object = new shapescollisionShape(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "abstractsquare.js") {
+        object = new abstractsquare(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "testcategoryshape.js") { 
-object = new testcategoryshape(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "shapescollisionShape.js") {
+        object = new shapescollisionShape(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
-else if(catAndName == "testcollisionShape.js") { 
-object = new testcollisionShape(idIndex, null, null, null, null);
- object.setDefaultForUninstantiatedParameters(canvas)
- object.setObjectName(catAndName.split(".")[0]);
- object.draw(context)
+    else if (catAndName == "testcategoryshape.js") {
+        object = new testcategoryshape(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
 
- idIndex++; 
-canvasObjects.push(object);}
+        idIndex++;
+        canvasObjects.push(object);
+    }
 
- }
+    else if (catAndName == "testcollisionShape.js") {
+        object = new testcollisionShape(idIndex, null, null, null, null);
+        object.setDefaultForUninstantiatedParameters(canvas)
+        object.setObjectName(catAndName.split(".")[0]);
+        object.draw(context)
+
+        idIndex++;
+        canvasObjects.push(object);
+    }
+
+}
--- MasterThesisPlatform/wwwroot/js/staticbuildmethods/staticbuildelements.js
+++ MasterThesisPlatform/wwwroot/js/staticbuildmethods/staticbuildelements.js
@@ -31,8 +31,34 @@ canvas.onmousemove = function (e) {
 canvas.addEventListener('click', function (e) {
     for (var i = 0; i < canvasObjects.length; i++) {
         if (canvasObjects[i].getCollisionArea(e)) {
+            //get and reset div
             var div = document.getElementById("buildmode-options-elements");
-            div.innerHTML = "<p>" + canvasObjects[i].object + "</p>";
+            div.innerHTML = "";
+
+            for (var j = 0; j < Object.keys(canvasObjects[i]).length; j++) {
+                //Oh boy this is unreadable as fuck
+                if (typeof Object.values(canvasObjects[i])[j] === 'object') {
+                    for (var k = 0; k < Object.values(canvasObjects[i])[j].length; k++) {
+                        div.innerHTML += "<p><strong> bound </strong></p>";
+                        div.innerHTML += "<p id=" + '"' + "objectvalue" + k + '"' + "><p>"
+                        for (var n = 0; n < Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k]).length; n++) {
+                            document.getElementById("objectvalue" + k).innerHTML +=
+                                Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + ": " +
+                                Object.values(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + ", ";
+                        }
+
+                    }
+                    console.log(Object.values(canvasObjects[i])[j]);
+                    //console.log(Object.values(canvasObjects[i])[j])
+                } else if (typeof Object.values(canvasObjects[i])[j] === 'boolean') {
+                    console.log("bool detected");
+                }
+
+            }
+
+            div.innerHTML += "<p>" + canvasObjects[i].object + "</p>";
+            div.innerHTML += "<input id=" + '"' + "boundsTextfield" + '"' + " type=" + '"' + "text" + '"' + "><br>";
+            div.innerHTML += "<input id=" + '"' + "boundsTextfield" + '"' + " type=" + '"' + "text" + '"' + "><br>";
         }
     }
 }, false)
