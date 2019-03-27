281a7acf267ac25e69c6a1d21258a994f44d20d6
Triah <nicolaihedegaardjensen93@gmail.com>
2019-03-27 03:32:00 +0100

    alot of really really hard to understand javascript that handles objects of unknown types additions in the option menu, not quite done yet but pretty close to done with that part of it

--- MasterThesisPlatform/wwwroot/js/gamerepresentation.js
+++ MasterThesisPlatform/wwwroot/js/gamerepresentation.js
@@ -6,6 +6,9 @@ var canvasObjects = []
 var lockedItem = null;
 var itemIsLocked = false;
 var idIndex = 0;
+var objectVariableToAddTo = null;
+var objectIdToAddTo = null;
+var keysForObject = [];
 
 canvas.onmousedown = function (e) {
     for (var i = 0; i < canvasObjects.length; i++) {
@@ -40,30 +43,79 @@ canvas.addEventListener('click', function (e) {
             for (var j = 0; j < Object.keys(canvasObjects[i]).length; j++) {
                 //Oh boy this is unreadable as fuck
                 if (typeof Object.values(canvasObjects[i])[j] === 'object') {
+                    objectVariableToAddTo = Object.keys(canvasObjects[i])[j];
+                    objectIdToAddTo = Object.values(canvasObjects[i])[0];
+                    div.innerHTML += "<p style=" + '"' + "text-align:center;" + '"' + "><strong><u>" + Object.keys(canvasObjects[i])[j] + "</u></strong></p>"
                     for (var k = 0; k < Object.values(canvasObjects[i])[j].length; k++) {
-                        div.innerHTML += "<p><strong> bound </strong></p>";
-                        div.innerHTML += "<p id=" + '"' + "objectvalue" + k + '"' + "><p>"
+                        div.innerHTML += "<p style=" + '"' + "text-align:center;" + '"' + "id=" + '"' + "objectvalue" + k + '"' + "><p>"
                         for (var n = 0; n < Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k]).length; n++) {
                             document.getElementById("objectvalue" + k).innerHTML +=
-                                Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + ": " +
-                                Object.values(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + ", ";
+                                Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + (k + 1) + ": " +
+                                Object.values(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + "<br>";
+                            if (k == Object.values(canvasObjects[i])[j].length - 1) {
+                                keysForObject[n] = Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k])[n];
+                                div.innerHTML += "<div style=" + '"' + "text-align:center; margin-bottom:3px;" + '"' + "> "
+                                    + " <form style=" + '"' + "display: inline-block;" + '"' + ">"
+                                    + "<input id=" + '"' + Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k])[n] +
+                                    (Object.values(canvasObjects[i])[j].length + 1) +'"' +
+                                    " style=" + '"' + "text-align:center;" + '"' + " type=" + '"' + "text" + '"' +
+                                    " placeholder=" + '"' + Object.keys(Object.values(Object.values(canvasObjects[i])[j])[k])[n] +
+                                    (Object.values(canvasObjects[i])[j].length + 1) + " of type: "
+                                    + typeof Object.values(Object.values(Object.values(canvasObjects[i])[j])[k])[n] + '"'
+                                    + "onchange=" + '"' + "newValuesOfObject[" + n +"]=" + "this.value;" + '"' 
+                                    + " /> "
+                                    + "</form></div>"
+                            }
                         }
-                        
                     }
-                    console.log(Object.values(canvasObjects[i])[j]);
-                    //console.log(Object.values(canvasObjects[i])[j])
+                    div.innerHTML += "<div style=" + '"' + "text-align:center; margin-bottom:3px;" + '"' + ">"
+                        + "<button onclick=" + '"' + "addToObjectValues(newValuesOfObject)" + '"' + ">"
+                        + "Add New Entry </button></div>"
+
+                    //Line separating the values
+                    div.innerHTML += "<hr style=" + '"' + "height:1px;border:none;color:#333;background-color:#333;" + '"' + ">";
                 } else if (typeof Object.values(canvasObjects[i])[j] === 'boolean') {
                     console.log("bool detected");
                 }
-
             }
-
-            div.innerHTML += "<p>" + canvasObjects[i].object + "</p>";
-            div.innerHTML += "<input id=" + '"' + "boundsTextfield" + '"' + " type=" + '"' + "text" + '"' + "><br>";
-            div.innerHTML += "<input id=" + '"' + "boundsTextfield" + '"' + " type=" + '"' + "text" + '"' + "><br>";
         }
     }
 }, false)
+
+function addToObjectValues(listOfValues) {
+    if (objectVariableToAddTo != null && objectIdToAddTo != null) {
+        var newObjectToAdd = {};
+        for (var i = 0; i < keysForObject.length; i++) {
+            if (Object.is(parseInt(listOfValues[i]), NaN)) {
+                newObjectToAdd[keysForObject[i]] = listOfValues[i];
+            } else {
+                //special case for bounds
+                newObjectToAdd[keysForObject[i]] = parseInt(listOfValues[i]);
+            }
+            
+        }
+        //make a special case for bounds which ensures that whenever a new bound is put in it is done to inbetween the nearest points
+        //this can be done by using the euclidian distance i guess
+        for (var i = 0; i < canvasObjects.length; i++) {
+            if (canvasObjects[i].id == objectIdToAddTo) {
+                for (var j = 0; j < Object.keys(canvasObjects[i]).length; j++) {
+                    if (Object.keys(canvasObjects[i])[j] == objectVariableToAddTo) {
+                        Object.values(canvasObjects[i])[j][Object.values(canvasObjects[i])[j].length] = newObjectToAdd;
+                        console.log(canvasObjects[i]);
+                    }
+                }
+            }
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            canvasObjects[i].draw(context);
+        }
+        
+        //set object name to null again
+        keysForObject = []
+        objectVariableToAddTo = null;
+        objectIdToAddTo = null;
+    }
+
+}
 
 canvas.onmouseup = function (e) {
     //unlock item
